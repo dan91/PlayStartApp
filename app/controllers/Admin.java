72ca6cf243679ef59c7a5ea76340d22dd7012adc
kosmitzki <mitch>
2014-07-07 11:18:50 +0200

    admin

---
+++ app/controllers/Admin.java
@@ -0,0 +1,66 @@
+package controllers;
+
+import models.User;
+import models.utils.AppException;
+import play.Logger;
+import play.data.Form;
+import play.data.validation.Constraints;
+import play.i18n.Messages;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+
+import static play.data.Form.form;
+
+/**
+ * Login and Logout.
+ * User: yesnault
+ */
+public class Admin extends Controller {
+
+    public static Result GO_HOME = redirect(
+            routes.Application.index()
+    );
+
+    public static Result GO_DASHBOARD = redirect(
+            routes.Dashboard.index()
+    );
+
+    /**
+     * Displays the admin mainview
+     *
+     * @return
+     */
+    public static Result admin() {  
+        return ok(views.html.admin.admin.render());
+    }
+
+
+        /**
+     * Displays the admin user view
+     *
+     * @return
+     */
+    public static Result astudies() {  
+        return ok(views.html.admin.studies.render());
+    }
+
+        /**
+     * Displays the admin studies
+     *
+     * @return
+     */
+    public static Result auser() {  
+        return ok(views.html.admin.user.render());
+    }
+        /**
+     * Displays the admin lab
+     *
+     * @return
+     */
+    public static Result alab() {  
+        return ok(views.html.admin.lab.render());
+    }
+
+
+}
