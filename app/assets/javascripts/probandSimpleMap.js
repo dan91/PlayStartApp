3e5f6021f4adf43c50ba37682f75f2d2a65475ab
Pinguinmann <Tom.Aussenhofer@gmx.de>
2014-07-12 12:49:13 +0200

    /registered verbessert + /template simpleMap

---
+++ app/assets/javascripts/probandSimpleMap.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    	
+    	var myCenter=new google.maps.LatLng(49.785210, 9.976927);
+    	var marker=new google.maps.Marker({
+    	position:myCenter
+    	});
+    	
+    	var map=    new google.maps.Map(
+                 document.getElementById('map-canvas'), 
+                  { zoom: 18,
+                    center: myCenter,
+                    mapTypeId: google.maps.MapTypeId.HYBRID });
+    	
+    	marker.setMap(map);
+    
+    	var contentString = '<div id="content">'+
+    	'<div id="siteNotice">'+
+    	'</div>'+
+    	'<h2 id="firstHeading" class="firstHeading">Gebäude 82</h2>'+
+    	'<div id="bodyContent">'+
+    	'<p><h6>Raum: 00.120</h6> befindet sich im ersten Stock'+
+    	'</br>des Gebäudes 52. Sie durchqueren den Flur </br>im ersten Stock'+
+    	' und gehen durch die Glastür.</br> Der Raum befindet sich'+
+    	'auf der rechten Seite.</p>'
+
+    	'</div>'+
+    	'</div>';
+
+    	var infowindow = new google.maps.InfoWindow({
+    	    content: contentString
+    	});
+    
+    $('#myMapModal').on('shown.bs.modal', function() {
+    	resizeShit();
+    	});
+    	
+    function resizeShit(){	
+    var center = map.getCenter();
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(center); 
+	
+	
+	
+	//var infowindow = new google.maps.InfoWindow({
+	//  content: contentString
+	//});
+
+	google.maps.event.addListener(marker, 'click', function() {
+	  infowindow.open(map,marker);
+	  });
+	
+	
+	infowindow.open(map,marker);
+    };
+    	
+    	
+    });
