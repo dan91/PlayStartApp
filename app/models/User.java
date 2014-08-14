c7e373e16e8a9c0ee081138676eb98d09db0f268
kosmitzki <mitch>
2014-08-14 18:09:37 +0200

    stmt

--- app/models/User.java
+++ app/models/User.java
@@ -57,6 +57,8 @@ public class User extends Model {
 		ResultSet rs = stmt
 				.executeQuery("SELECT name FROM User WHERE id = "+id+"");
 		rs.next();
+			stmt.close();
+
 		return rs.getString("name");
     }
     
