f2d613c55d5a7338d7058ff59113f469fa5bb0e5
Pinguinmann <Tom.Aussenhofer@gmx.de>
2014-08-23 11:09:17 +0200

    /completedMapper

---
+++ app/models/CompletedMapper.java
@@ -0,0 +1,17 @@
+package models;
+
+public class CompletedMapper {
+
+	public String experiment_name;
+	
+	public String date;
+	
+	public String time;
+	
+	public String end_time;
+	
+	public float proband_hours;
+	
+	public float duration;
+	
+}
