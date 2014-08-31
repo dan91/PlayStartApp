bf62de67997b3cada1e308cbcb71c87d4824f9fb
Daniel Diethei <dan.diethei@gmail.com>
2014-08-31 13:52:56 +0200

    user

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,32 +1,41 @@
 
-
 function updateUsers() {
-	
-	myJsRoutes.controllers.Experimenter.searchUsers().ajax({
 
-		method: 'post',
-		data: $(".form-horizontal").serialize(),
-		success : function(data) {
-			$("#users").html("")
-			var obj = jQuery.parseJSON(data);
-			console.log(obj)
-			$("#amount").text(obj.length)
-			$.each( obj, function( key, value ) {
-				  el = $("#user_dr").clone();
-				  el.show()
-				  el.find(".name").text(value.name);
-				  el.data("user-id", value.id);
-				  el.find("#edit").prop('href','edit/'+value.id)
-				  $("#users").append(el);
-				});
-		}
-		
-	});
+	myJsRoutes.controllers.Experimenter
+			.searchUsers()
+			.ajax(
+					{
+
+						method : 'post',
+						data : $(".form-horizontal").serialize(),
+						success : function(data) {
+							if (data == null)
+								$("#amount")
+										.text(
+												"Es gab leider keine Treffer für die angegebenen Kriterien.")
+							else {
+								$("#users").html("")
+								var obj = jQuery.parseJSON(data);
+								console.log(obj)
+								$("#amount").text(obj.length)
+								$.each(obj, function(key, value) {
+									el = $("#user_dr").clone();
+									el.show()
+									el.find(".name").text(value.name);
+									el.data("user-id", value.id);
+									el.find("#edit").prop('href',
+											'user_edit/' + value.id)
+									$("#users").append(el);
+								});
+							}
+						}
+
+					});
 }
 
-	$(document).on('ready', function () {
-		updateUsers();
-	})
-	$( "form :input, :checkbox" ).change(function() {
-		updateUsers();
-	});
+$(document).on('ready', function() {
+	updateUsers();
+})
+$("form :input, :checkbox").change(function() {
+	updateUsers();
+});
